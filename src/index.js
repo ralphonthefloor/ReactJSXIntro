c11391fcfc6caf01b9ca680ff623cc12b800730b
ralphonthefloor <95453329+ralphonthefloor@users.noreply.github.com>
2022-05-12 17:23:40 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Matt";
+
 ReactDOM.render(
   <div id="greeting">
-    <h1>Hello</h1>
+    <h1>Hello, my name is {name}</h1>
     <p>This is a paragraph!!!</p>
   </div>,
   document.getElementById("root")
