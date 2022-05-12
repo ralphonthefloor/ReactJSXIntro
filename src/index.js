69cd40a23e54d2966a319b650cf4688130cc3b71
ralphonthefloor <95453329+ralphonthefloor@users.noreply.github.com>
2022-05-12 17:19:09 -0400

    some changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph.</p>
+  <div id="greeting">
+    <h1>Hello</h1>
+    <p>This is a paragraph!!!</p>
   </div>,
   document.getElementById("root")
 );
